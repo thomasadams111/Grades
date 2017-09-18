71e8b3ed6d4bf8a08d3c4f822bd1d1ac6dde63a0
Thomas Adams <thomas.adams.111@outlook.com>
2017-09-18 19:46:53 +0100

    Project status after completing the Classes and Objects in C# module.

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        // constructors
+        public GradeBook()
+        {
+            _grades = new List<float>();
+        }
+
+        // methods
+        public void AddGrade(float grade)
+        {
+            if (grade >= 0 && grade <= 100 )
+            {
+                _grades.Add(grade);
+            }
+        }
+        
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0f;
+            foreach (float grade in _grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / _grades.Count;
+
+            return stats;
+        }
+
+        // fields
+        private List<float> _grades;
+    }
+}
---
+++ Grades/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook(); //Instatiate gradebook class, using constructor.
+            book.AddGrade(91f);
+            book.AddGrade(89.1f);
+            book.AddGrade(75f);
+
+            GradeStatistics stats = book.ComputeStatistics(); // return statistics object
+            Console.WriteLine($"Average grade: {stats.AverageGrade}");
+            Console.WriteLine($"Highest grade: {stats.HighestGrade}");
+            Console.WriteLine($"Lowest grade: {stats.LowestGrade}");
+
+
+
+        }
+    }
+}
